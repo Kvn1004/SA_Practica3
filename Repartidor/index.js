8f71abc0abef4dc31708fab1627e33d0b465bf66
Kvn1004 <pex089@gmail.com>
2020-08-28 23:02:11 -0600

    pedido entregado

--- Repartidor/index.js
+++ Repartidor/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/pedido', function(req, res) {
-    res.send("Llevo su pedido en camino");
+    res.send("Su pedido está entregado");
 });
 
 app.get('/', function(req, res) {
